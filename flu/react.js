1320baa6a6da27a9e5b5995964205a83d1796235
joaorodolpho <110494362+joaorodolpho@users.noreply.github.com>
2022-08-06 14:28:52 -0300

    mudei o react

--- flu/react.js
+++ flu/react.js
@@ -1,9 +1,9 @@
-
+import React from 'React';
 function CapsLock(props){
     const textoInserido = props.texto;
     const textoEmCapsLock = textoInserido.toUpperCase();
     return <div>{textoEmCapsLock}</div>
 }
 function pagina() {
-    return <CapsLock texto=''cu" />
+return <CapsLock texto="cu"/>
 }
