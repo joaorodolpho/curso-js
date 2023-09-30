647f41e7a584c1ac6eb85c88e8f1e8a8bcb921ec
joaorodolpho <110494362+joaorodolpho@users.noreply.github.com>
2023-09-30 14:24:36 -0300

    s
    cicada

--- flu/cicada.js
+++ flu/cicada.js
@@ -4,7 +4,7 @@ function validarLogin() {
     var mensagem = document.getElementById("mensagem");
 
     if (login === "CICADA" && senha === "3301") {
-        mensagem.innerHTML = "Login bem-sucedido!";
+        mensagem.innerHTML = "Login bem-sucedido!FALE COMIGO NO PV! (vc é esperto não é mesmo?";
         mensagem.style.color = "green";
     } else {
         mensagem.innerHTML = "Login ou senha incorretos. Tente novamente.";
